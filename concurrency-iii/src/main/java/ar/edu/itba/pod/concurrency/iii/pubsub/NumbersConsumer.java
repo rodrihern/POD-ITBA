8ee55bb89ed0a218d84b50995365edcdeed6c4b5
Rodrigo Alejandro Hernandez <rohernandez003@gmail.com>
2026-08-25 09:16:04 -0300

    ejercicios en clase

--- concurrency-iii/src/main/java/ar/edu/itba/pod/concurrency/iii/pubsub/NumbersConsumer.java
+++ concurrency-iii/src/main/java/ar/edu/itba/pod/concurrency/iii/pubsub/NumbersConsumer.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.pod.concurrency.iii.pubsub;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 public class NumbersConsumer implements Runnable {
@@ -13,12 +14,15 @@ public class NumbersConsumer implements Runnable {
 
     public void run() {
         try {
+            int sum = 0;
             while (true) {
                 Integer number = queue.take();
                 if (number.equals(poisonPill)) {
+                    System.out.println(Thread.currentThread().getName() + " result: " + sum);
                     return;
                 }
-                System.out.println(Thread.currentThread().getName() + " result: " + number);
+                // System.out.println(Thread.currentThread().getName() + " result: " + number);
+                sum += number;
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
--- concurrency-iii/src/test/java/ar/edu/itba/pod/concurrency/iii/e3/SortBenchmark.java
+++ concurrency-iii/src/test/java/ar/edu/itba/pod/concurrency/iii/e3/SortBenchmark.java
@@ -6,6 +6,7 @@ import java.util.function.Consumer;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,9 +17,44 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class SortBenchmark {
 
-    @Test
+    @RepeatedTest(4)
     public void benchmark_all() {
+        final Consumer<int[]> serial = a -> Arrays.sort(a);
+        final Consumer<int[]> parallel = a -> Arrays.parallelSort(a);
+        final int multiplier = 100_000;
+        final Random random = new Random();
 
+        final int[] small = generateArray(10 * multiplier, random);
+        final int[] med = generateArray(25 * multiplier, random);
+        final int[] large = generateArray(50 * multiplier, random);
+
+        benchmark(small, serial, "Small serial");
+        benchmark(small, parallel, "Small parallel");
+        benchmark(med, serial, "med serial");
+        benchmark(med, parallel, "med parallel");
+        benchmark(large, serial, "large serial");
+        benchmark(large, parallel, "large parallel");
+    }
+
+    private int[] generateArray(final int size, final Random random) {
+        final int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt();
+        }
+
+        return array;
+    }
+
+    private void benchmark(final int[] array, final Consumer<int[]> orderer, final String message) {
+        long accumulate = 0;
+        for (int i = 0; i < 4; i++) {
+            final long startTime = System.currentTimeMillis();
+            orderer.accept(Arrays.copyOf(array, array.length));
+            final long finishTime = System.currentTimeMillis();
+            accumulate += finishTime - startTime;
+        }
+
+        System.out.println(message + " took " + (accumulate / 4) + " ms");
     }
 
 }
